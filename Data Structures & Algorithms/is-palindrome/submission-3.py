d2c21479322eaa26f7a46fd09ee68be904148227
Vijay M H <vijaymh041@gmail.com>
2026-06-17 00:52:21 +0530

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        arr=[]
+        for i in range(0,len(s)):
+            if s[i]!=' ' and ord(s[i])>64 and ord(s[i])<123 and s[i].isalnum():
+                if ord(s[i])>96:
+                    arr.append(chr(ord(s[i])-32))
+                else:
+                    arr.append(s[i])
+
+        if arr[:]==arr[::-1]:
+            return True
+        else:
+            return False
+            
+
+
+
+        
